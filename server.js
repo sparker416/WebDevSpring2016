750a12d2fe2cb93498d8394de98ee82c307586fb
sparker416 <sparker416@gmail.com>
2016-07-12 18:33:51 -0400

    updated app.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ console.log(db);
 
 var app = express();
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || "3000";
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); // for parsing application/json
