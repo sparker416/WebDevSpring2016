9266e349f87e82c8c60390b14164e5c4da0beaa0
sparker416 <sparker416@gmail.com>
2016-10-07 21:48:02 -0400

    updated pricing

--- public/project/client/controllers/contact.controller.js
+++ public/project/client/controllers/contact.controller.js
@@ -6,8 +6,15 @@
         .module("KnightMovesApp")
         .controller("ContactController", ContactController);
 
-    function ContactController($location) {
+    function ContactController($location, $window) {
         var vm = this;
         vm.$location = $location;
+
+        vm.mail = mail;
+
+        function mail(name, email, message){
+            $window.location="mailto:sparker416@gmail.com?subject=Message%20from%20" + name + "%20at%20" + email +
+                "&body=" +message;
+        }
     }
 })();
